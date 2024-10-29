84e4fe1cdf070ca6a57f60cbef8a91bd5998c553
LAURA PEREIRA AMARAL <52233446852@info.local>
2024-10-29 14:21:06 -0300

    Atualizando

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af40e4c99239474c5f78102567c0c3204d9a1e71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce216aa1118dfbb0cb1e476204985702f97b563")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
