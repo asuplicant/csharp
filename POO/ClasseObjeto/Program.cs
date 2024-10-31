f41c07ee4c7d8568b58770c483c78b9b6c7b7f16
LAURA PEREIRA AMARAL <52233446852@info.local>
2024-10-31 16:45:24 -0300

    ClasseObjeto

---
+++ POO/ClasseObjeto/Program.cs
@@ -0,0 +1,31 @@
+﻿// Importando as classes de dentro da pasta Classes
+using ClasseObjeto.Classes;
+
+// Cria o objeto/variável animal
+Animal cachorro = new Animal();
+
+cachorro.nome = "Yumi";
+cachorro.raca = "Shih Tzu";
+cachorro.cor = "Marrom, preto e branco";
+cachorro.idade = 9;
+
+Console.WriteLine($"Nome da cachorrinha: {cachorro.nome}");
+Console.WriteLine($"Raça da cachorrinha: {cachorro.raca}");
+Console.WriteLine($"Cor da cachorrinha: {cachorro.cor}");
+Console.WriteLine($"Idade da cachorrinha: {cachorro.idade}");
+
+Animal gato = new Animal();
+
+Console.WriteLine("");
+
+gato.nome = "Ami";
+gato.raca = "Siamês";
+gato.cor = "Marrom";
+gato.idade = 10;
+
+Console.WriteLine($"Nome da gatinha: {gato.nome}");
+Console.WriteLine($"Raça da gatinha: {gato.raca}");
+Console.WriteLine($"Cor da gatinha: {gato.cor}");
+Console.WriteLine($"Idade da gatinha: {gato.idade}");
+
+
